13c6160573a840518627cda536b4613101f420e8
akjoshi12 <60343159+akjoshi12@users.noreply.github.com>
2024-03-19 10:49:41 -0400

    Updated home.py - Version 0.0.1

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ from object_det_app import *
 model_OCR = YOLO('yolov8n.pt')
 model = YOLO('yolov8n.pt')
 
-def padestrian_mode(uploaded_file):
+def padestrian_mode(uploaded_file,conf):
     st.empty()
     st.write('pedestrian mode')
     #results = model(source="bike_r.mp4", show=True, conf=0.4, save=True, project='streamlit') 
@@ -17,13 +17,13 @@ def padestrian_mode(uploaded_file):
         st.write("File Uploaded")  
         f_path = uploaded_file.name
 
-        obj_detection(f_path)
+        obj_detection(f_path,conf/100)
  
     else:
         st.write("Please upload a video file.")
 
 
-def alert_mode(uploaded_file):
+def alert_mode(uploaded_file,conf):
     st.empty()
     st.write('alert mode')
 
@@ -32,7 +32,7 @@ def alert_mode(uploaded_file):
 
         f_path = uploaded_file.name
         st.write('File Uploaded')
-        res = alert(f_path)
+        res = alert(f_path,conf/100)
 
         obj_list = ['car','cycle','truck','bus','bike']
         print(res)
@@ -47,7 +47,7 @@ def alert_mode(uploaded_file):
 
 
 
-def OCR(uploaded_file):
+def OCR(uploaded_file,conf_level):
     st.empty()
     st.write('OCR mode')
 
@@ -57,7 +57,7 @@ def OCR(uploaded_file):
         f_path = uploaded_file.name
 
         vid_cap = cv2.VideoCapture(f_path)
-        detected_text = main_func(vid_cap, model_OCR, 0.4)
+        detected_text = main_func(vid_cap, model_OCR, conf_level/100)
         st.write("Most common text:", detected_text)
 
     else:
@@ -66,16 +66,21 @@ def OCR(uploaded_file):
 
 
 if __name__ == "__main__":
-    st.write("Welcome to ThirdEye")
+    st.header(" Welcome to Third👁️Eye")
+    st.write("_________________________________________________________")
+
     uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
     mode = st.radio("Select mode", ["Padestrian Mode", "Alert Mode", "OCR Mode"])
 
     if mode == 'Padestrian Mode':
-        padestrian_mode(uploaded_file)
+        conf_level = st.slider('Confidence Level', 0, 100, 25)
+        padestrian_mode(uploaded_file,conf_level)
 
     if mode == 'Alert Mode':
-        alert_mode(uploaded_file)
+        conf_level = st.slider('Confidence Level', 0, 100, 25)
+        alert_mode(uploaded_file,conf_level)
 
     if mode == 'OCR Mode':
-        OCR(uploaded_file)
+        conf_level = st.slider('Confidence Level', 0, 100, 25)
+        OCR(uploaded_file,conf_level)
 
